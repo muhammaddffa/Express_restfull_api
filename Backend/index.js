c9c3dac86da6bbec6e536b4cad27c767dfb93896
muhammadffa <muhammaddaffa463@gmail.com>
2024-08-12 21:26:48 +0700

    Done CRUD

--- Backend/index.js
+++ Backend/index.js
@@ -2,15 +2,19 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import ProductRoute from "./routes/ProductRoute.js";
-dotenv.config();
+
 
 const app = express();
+
+// const PORT = process.env.PORT;
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 app.use(ProductRoute);
 
 
-app.listen(process.env.APP_PORT, () => {
-    console.log('Server up and running...');
+app.listen(process.env.PORT, () => {
+    console.log(`Server up and running on port: ${process.env.PORT}`);
 });
